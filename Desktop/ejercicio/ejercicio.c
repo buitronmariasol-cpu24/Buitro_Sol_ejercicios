4480964ae395fc6e60db4d89ad9a3255c77c8915
buitronmariasol-cpu24 <maria.buitron.portilla@udla.edu.ec>
2026-05-18 12:11:16 -0500

    Segundo commit: creacion de prototipo

--- Desktop/ejercicio/ejercicio.c
+++ Desktop/ejercicio/ejercicio.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+float calPromedio(float *vector, int tamanio);
 
 int main()
 {
-    float v[5]={6,7,7.5,6.5,8};
+    float notas[5]={6,7,7.5,6.5,8};
+    int tamanio=5;
+    
     for (int i =0; i<5; i++)
     {
-        printf("%2.f",v[i]);
+        printf("%2.f",notas[i]);
     }
 
+    calPromedio(notas, tamanio);
 }
+
